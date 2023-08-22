24886bea0244eeaf764587c7fb1b2af392070e5c
Nico <nico@beyer-soehne.de>
2023-08-22 13:34:57 +0200

    - shopify_order

--- src/helper/transformations.ts
+++ src/helper/transformations.ts
@@ -2,10 +2,10 @@
 
 export function addFieldsValidateWebhook() {
     return {$addFields: {
-            hmac: {$hmac: ["$body", "256c77cef0c1227c721d8d7494b1ff4a", "sha256", "base64"]},
+            hmac: {$hmac: ["$body", "687c555cbadf46d39c52b7079da5e53f", "sha256", "base64"]},
             _validWebhook: {
                 $eq: [
-                    {$hmac: ["$body", "256c77cef0c1227c721d8d7494b1ff4a", "sha256", "base64"]},
+                    {$hmac: ["$body", "687c555cbadf46d39c52b7079da5e53f", "sha256", "base64"]},
                     "$headers.X-Shopify-Hmac-Sha256"
                 ]
             },
--- src/subprocesses/webhook_reset.ts
+++ src/subprocesses/webhook_reset.ts
@@ -1,5 +1,65 @@
-import {ProcessCreator} from "@nbeyer/pms-process-creator";
+import {ProcessCreator, ServiceInstance} from "@nbeyer/pms-process-creator";
+import {Delay} from "@nbeyer/pms2-delay";
+import {Noop} from "@nbeyer/pms-noop";
+import {ApiCallMessage} from "@nbeyer/pms2-shopify/lib/src/model/messages/input/ApiCall";
 
-export function processOrders(pc: ProcessCreator) {
+export function webhook_reset(pc: ProcessCreator) {
+
+    pc.addInstance(new ServiceInstance<Delay>(Delay, {
+        instanceName: "audit-webhook-reset",
+    }))
+    .addInstance(new ServiceInstance<Noop>(Noop, {
+        instanceName: "audit-webhook-reset-noop",
+    }))
+    .connectInstance("audit-webhook-reset-noop", "pms2-shopify", {
+        type: "SQSQueue",
+        transformation: [
+            {$match: {
+                "type": "webhook-check"
+            }},
+            {$project: {
+                type: "ApiCall",
+                method: "get",
+                path: "webhooks.json",
+            } as Partial<ApiCallMessage>}
+        ]
+    })
+    .connectInstance("pms2-shopify", "pms2-shopify", {
+        type: "SQSQueue",
+        transformation: [
+            {$match: {
+                webhooks: {$ne: null}
+            }},
+            {$addFields: {
+                hasWebhook: {$reduce: {
+                    input: "$webhooks",
+                    initialValue: false,
+                    in: {
+                        $or: ["$$value", {
+                            $and: [
+                                {$eq: ["$$this.address", "https://api.beyer-soehne.de/pms2-events/pms2-shopify-audit/order"]},
+                                {$eq: ["$$this.topic", "orders/update"]},
+                            ]
+                        }]
+                    }
+                }}
+            }},
+            {$match: {
+                hasWebhook: false
+            }},
+            {$project: {
+                type: "ApiCall",
+                method: "post",
+                path: "webhooks.json",
+                body: {
+                    webhook: {
+                        topic: "orders/update",
+                        address: "https://api.beyer-soehne.de/pms2-events/pms2-shopify-audit/order",
+                        format: "json"
+                    }
+                }
+            } as Partial<ApiCallMessage>}
+        ]
+    })
 
 }
